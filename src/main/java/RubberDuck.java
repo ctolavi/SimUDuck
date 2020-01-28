7ae450945ec3712d0a5b2c212b7ba06cb1f0c37d
Crispin Tolavi <cris.emis@gmail.com>
2020-01-27 20:48:17 -0400

    adding rubberDucks that don't quack

---
+++ src/main/java/RubberDuck.java
@@ -0,0 +1,13 @@
+public class RubberDuck extends Duck {
+
+    @Override
+    void quack() {
+        System.out.println("squeak");
+    }
+
+    void display() {
+        System.out.println("looks like a rubberduck");
+    }
+
+
+}
